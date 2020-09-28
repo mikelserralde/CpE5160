df20e72a522e369a4c458a85a86acf5e501061b5
Mikel Serralde <mikelserralde@gmail.com>
2020-09-28 18:19:28 -0500

    Refresh :+1:

---
+++ experiment_one/experiment_one/main.c
@@ -0,0 +1,16 @@
+/*
+ * experiment_one.c
+ * Authors : Mikel Serralde & Hayden B. Long
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
