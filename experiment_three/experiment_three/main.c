d581681a8912fdcf6825025d2a83010bb0a4c9c1
Mikel Serralde <mikelserralde@gmail.com>
2020-11-06 18:01:41 -0600

    Added Project Three

---
+++ experiment_three/experiment_three/main.c
@@ -0,0 +1,18 @@
+/*
+ * experiment_three.c
+ *
+ * Created: 11/6/2020 6:00:58 PM
+ * Author : Mikel
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
