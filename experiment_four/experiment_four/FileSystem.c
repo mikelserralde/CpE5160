f96308cb740c7b1b5158203e4b84b322df6dea7f
Mikel Serralde <mesntf@mst.edu>
2020-11-29 19:19:08 -0600

    Progress on the MOunt DriVE fuNcTiON
    Con amor, para ti Hayden Long.

--- experiment_four/experiment_four/FileSystem.c
+++ experiment_four/experiment_four/FileSystem.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "FileSystem.h"
+#include "SDCard.h"
+#include "Directory_Functions_struct.h"
 
 uint8_t read_value_8(uint16_t offset, uint8_t *array_name)
 {
@@ -20,4 +22,13 @@ uint32_t read_value_32(uint16_t offset, uint8_t *array_name)
 	uint32_t temp32;
 	temp32 = *(array_name+offset);
 	return temp32;
+}
+
+//uint8_t mount_drive(void);
+uint8_t mount_drive(uint8_t * array)
+{
+//	uint8_t array??
+	// Step 1: Read Block 0
+	Read_Block(0, array?);
+	
 }
