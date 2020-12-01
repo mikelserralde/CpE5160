86ca4d3f2fc0e986bc06c31d5955193f89d1fd58
Mikel Serralde <mesntf@mst.edu>
2020-12-01 16:34:58 -0600

    Update FileSystem.h

--- experiment_four/experiment_four/FileSystem.h
+++ experiment_four/experiment_four/FileSystem.h
@@ -19,7 +19,7 @@
 
 // ERRORS
 #define BPB_NOT_FOUND 0xCC
-#define SUCCESS = 0xFF;
+#define SUCCESS 0xFF
 
 
 uint8_t read_value_8(uint16_t offset, uint8_t *array_name);
@@ -27,5 +27,11 @@ uint8_t read_value_8(uint16_t offset, uint8_t *array_name);
 uint16_t read_value_16(uint16_t offset, uint8_t *array_name);
 
 uint32_t read_value_32(uint16_t offset, uint8_t *array_name);
+
+uint8_t mount_drive(uint8_t * array);
+
+uint32_t First_Sector(uint32_t cluster_num);
+
+uint32_t Find_Next_Clus(uint32_t cluster_num, uint8_t * array);
 
 #endif //FILE_SYSTEM_H_
